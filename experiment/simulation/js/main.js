d6c0e8edb9eee83b2fc50582d6ca240d4cd596b4
prakriti5dhang <prakritidhang@gmail.com>
2023-06-23 13:12:29 +0530

    pytest updated

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -4,9 +4,6 @@ Exp:
 File Name: main.js
 Author: Prakriti Dhang*/
 
-
-
-
 $(document).ready(function() {
   $('.navbar-nav .nav-link').click(function() {
     $('.navbar-nav .nav-link').removeClass('active'); // Remove active class from all links
